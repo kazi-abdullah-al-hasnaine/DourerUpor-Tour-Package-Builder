3fd4ae1cac84c8487b3954d7a6e0acbc09532538
Tahshan Jamil Shadhin <tj.shadhin001@gmail.com>
2025-03-27 17:51:33 +0600

    Fixed Build & Share section on home page

--- Shadhin-experiment/decoration.php
+++ Shadhin-experiment/decoration.php
@@ -6,6 +6,7 @@
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<!-- Css file link -->
-	<link rel="stylesheet" type="text/css" href="styles.css">
+	<link rel="stylesheet" type="text/css" href="styles.css?v=<?php echo time(); ?>">
+	<!-- <link rel="stylesheet" type="text/css" href="styles.css"> -->
 </head>
 </html>
--- Shadhin-experiment/modules/buildPackages.php
+++ Shadhin-experiment/modules/buildPackages.php
@@ -1,3 +1,12 @@
-<section>
-	<h1>Build Packages</h1>
-</section>
+<body>
+	<section id="build-and-share-section">
+		<div class="title-container"><h1>Build & share</h1></div>
+		<div>
+			<p>Share your experiances with the amazing community of tourists and explorers!</p>
+		</div>
+		<div>
+			<button class="theme-btn">Build & Publish
+			</button>
+		</div>
+	</section>	
+</body>
