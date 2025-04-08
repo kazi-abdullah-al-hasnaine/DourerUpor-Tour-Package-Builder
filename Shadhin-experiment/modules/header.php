d23a75e30a987ef308f942aaa57097a81860b87e
KaziAbdullah <119121503+Hasnaine2000@users.noreply.github.com>
2025-04-09 02:16:20 +0600

    Chat working with Limit

--- Shadhin-experiment/modules/header.php
+++ Shadhin-experiment/modules/header.php
@@ -91,6 +91,9 @@ if (isset($_SESSION['email'])) {
 <?php 
     include "wishlist.php";
 ?>
+<script>
+(function(){if(!window.chatbase||window.chatbase("getState")!=="initialized"){window.chatbase=(...arguments)=>{if(!window.chatbase.q){window.chatbase.q=[]}window.chatbase.q.push(arguments)};window.chatbase=new Proxy(window.chatbase,{get(target,prop){if(prop==="q"){return target.q}return(...args)=>target(prop,...args)}})}const onLoad=function(){const script=document.createElement("script");script.src="https://www.chatbase.co/embed.min.js";script.id="YrrEV-HmhhBJr_IDWk8u-";script.domain="www.chatbase.co";document.body.appendChild(script)};if(document.readyState==="complete"){onLoad()}else{window.addEventListener("load",onLoad)}})();
+</script>
 </body>
 
 </html>
