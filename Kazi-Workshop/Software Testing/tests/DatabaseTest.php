ae8b1106d2e0df51452ae7b51228a34b8f6661ff
KaziAbdullah <119121503+Hasnaine2000@users.noreply.github.com>
2025-04-15 14:32:06 +0600

    Software Testing New Line

--- Kazi-Workshop/Software Testing/tests/DatabaseTest.php
+++ Kazi-Workshop/Software Testing/tests/DatabaseTest.php
@@ -1,6 +1,8 @@
 <?php
 // filepath: c:\xampp\htdocs\DourerUpor-Tour-Package-Builder\tests\DatabaseTest.php
 
+// Starting database tests
+
 
 use PHPUnit\Framework\TestCase;
 
@@ -10,16 +12,8 @@ class DatabaseTest extends TestCase {
     public function testSingletonInstance() {
         $db1 = Database::getInstance(); // database 1
         $db2 = Database::getInstance(); // database 1
-
         // Assert that both instances are the same
         $this->assertSame($db1, $db2);
     }
 
-    public function testConnectionIsPDO() {
-        $db = Database::getInstance();
-        $conn = $db->getConnection();
-
-        // Assert that the connection is a PDO instance
-        $this->assertInstanceOf(PDO::class, $conn); // নিশ্চিত করে যে $conn একটি PDO অবজেক্ট। 
-    }
 }
