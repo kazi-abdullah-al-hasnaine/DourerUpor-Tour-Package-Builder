75b8ff142eb3a6cb45f2ae9f674a2ce072a1c448
KaziAbdullah <119121503+Hasnaine2000@users.noreply.github.com>
2025-05-07 21:08:53 +0600

    Folder Sorted

--- Umo Banik/modules/footer.php
+++
@@ -1,9 +0,0 @@
-<section id="footer">
-	<div class="ft-container">
-		<div class="row">
-			<div class="">
-				<p>&copy; 2025 DourerUpor. All rights reserved. <a href="../index.html">Learn more about the team</a></p>
-			</div>
-		</div>
-	</div>	
-</section>
